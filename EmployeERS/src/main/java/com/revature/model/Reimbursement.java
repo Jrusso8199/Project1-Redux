d6b851da03ae4d5b211cb72346fb8c65aee248af
Jason Russo <jrusso8199@gmail.com>
2021-09-15 01:08:44 -0400

    EOD 9/14/21

--- EmployeERS/src/main/java/com/revature/model/Reimbursement.java
+++ EmployeERS/src/main/java/com/revature/model/Reimbursement.java
@@ -4,6 +4,8 @@ import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,17 +19,33 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "reimbursement")
 
 public class Reimbursement {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private double amount;
+	@Column
 	private Date submitedDate;
+	@Column
 	private Date resolvedDate;
+	@Column
 	private String description;
+	@Column
 	private Long author;
+	@Column
 	private Long resolver;
+	@Enumerated(EnumType.STRING)
 	private ReimbursementStatus status;
+	@Enumerated(EnumType.STRING)
 	private ReimbursementType type;
 	
 	
